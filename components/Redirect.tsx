cc693756a1576e4fa0eb8922e783531db748620e
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2023-02-05 22:33:21 +0530

    Fixing redirect

--- components/Redirect.tsx
+++ components/Redirect.tsx
@@ -4,13 +4,13 @@ const Redirect = ({ redirect_to }: { redirect_to: string }) => {
   return (
     <div>
       <Head>
-        <link rel="canonical" href="{redirect_to}" />
+        <link rel="canonical" href={redirect_to} />
         <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
-        <meta httpEquiv="refresh" content="0;url={redirect_to}" />
+        <meta httpEquiv="refresh" content={`0;url=${redirect_to}`} />
       </Head>
       <h1>Redirecting...</h1>
-      <a href="{redirect_to}">Click here if you are not redirected.</a>
-      <script>location='{redirect_to}'</script>
+      <a href={redirect_to}>Click here if you are not redirected.</a>
+      <script>location=`${redirect_to}`</script>
     </div>
   );
 };
