715a94ac65ac8b781bdff18208f6c479719d3da7
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2023-02-05 23:27:04 +0530

    fixing broken links

--- pages/apps/it-regime-comparison/for-financial-year-2021-2022.tsx
+++ pages/apps/it-regime-comparison/for-financial-year-2021-2022.tsx
@@ -1,4 +1,4 @@
-import ItRegimePage from 'modules/it-regime/_page';
+import ItRegimePage from 'modules/it-regime/page';
 
 const ItRegime2021_2022 = () => <ItRegimePage fy="2021-2022" />;
 export default ItRegime2021_2022;
--- pages/apps/it-regime-comparison/for-financial-year-2022-2023.tsx
+++ pages/apps/it-regime-comparison/for-financial-year-2022-2023.tsx
@@ -1,4 +1,4 @@
-import ItRegimePage from 'modules/it-regime/_page';
+import ItRegimePage from 'modules/it-regime/page';
 
 const ItRegime2022_2023 = () => <ItRegimePage fy="2022-2023" />;
 export default ItRegime2022_2023;
