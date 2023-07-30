bec5f5dc1da54776718c57235c4f133c0790e626
Vijayaraghavan R <rvnotes127@gmail.com>
2023-07-30 20:39:18 +0530

    Fixed decimal formatting

--- modules/dr-calculator/components/Result.tsx
+++ modules/dr-calculator/components/Result.tsx
@@ -19,34 +19,55 @@ function Result({
     <div className="border border-gray-400 dark:border-gray-500 rounded p-2">
       <div className="p-2">
         <div>
-          Basic Pension:
+          Basic Pension :{' '}
           <span className="font-semibold">₹ {Math.round(basicPension).toLocaleString(locale)}</span>
         </div>
       </div>
       <div className="p-2">
         <div>
-          Retirement Year:
-          <span className="font-semibold">{retirementYear}</span>
+          Retirement Year : <span className="font-semibold">{retirementYear}</span>
         </div>
       </div>
       <div className="p-2">
         <div>
-          Commuted Portion: <span className="font-semibold">{commutation}</span>
+          Commuted Portion : <span className="font-semibold">{commutation}</span>
         </div>
       </div>
       <hr className="my-2 border-gray-400 dark:border-gray-500" />
 
       <div className="p-2">
         Dearness Relief with effect from Aug 2023 :{' '}
-        <span className="font-semibold">₹ {currentDr.toLocaleString(locale)}</span>
+        <span className="font-semibold">
+          ₹{' '}
+          {currentDr.toLocaleString(locale, {
+            style: 'decimal',
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          })}
+        </span>
       </div>
 
       <div className="p-2">
         Gross Pension (Including exgratia ₹800 or ₹450 as applicable) :{' '}
-        <span className="font-semibold">₹ {grossPension.toLocaleString(locale)}</span>
+        <span className="font-semibold">
+          ₹{' '}
+          {grossPension.toLocaleString(locale, {
+            style: 'decimal',
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          })}
+        </span>
       </div>
       <div className="p-2">
-        Net Pension : <span className="font-semibold">₹ {netPension.toLocaleString(locale)}</span>
+        Net Pension :{' '}
+        <span className="font-semibold">
+          ₹{' '}
+          {netPension.toLocaleString(locale, {
+            style: 'decimal',
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          })}
+        </span>
       </div>
     </div>
   );
