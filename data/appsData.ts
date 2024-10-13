d56d1f1d51431d1615faf717516ed42ad32de3b3
rviju <60571080+rviju@users.noreply.github.com>
2024-10-13 08:38:38 +0530

    Fixed broken link

--- data/appsData.ts
+++ data/appsData.ts
@@ -1,15 +1,15 @@
 const appsData = [
   {
-    title: 'IT Regime Comparer',
+    title: "IT Regime Comparer",
     description: `You can compare and pick which IT (new or old) regime will help save you tax outgo using this simple app. Key in your total income, annual deductions and your age group to find out if you can save tax with new IT regime or the old IT regime.`,
-    imgSrc: '/static/images/it-regime.png',
-    href: '/apps/it-regime-comparison/for-financial-year-2023-2024',
+    imgSrc: "/static/images/it-regime.png",
+    href: "/apps/it-regime-comparison/for-financial-year-2024-2025",
   },
   {
-    title: 'Dearness Relief Calculator for Bank Pensioners',
+    title: "Dearness Relief Calculator for Bank Pensioners",
     description: `You can calculate applicable DR (Dearness Relief) with effect from Aug 2023 using this DR calculator for bank pensioners / family pensioners`,
-    imgSrc: '/static/images/dr-calculator.png',
-    href: '/apps/dr-calculator',
+    imgSrc: "/static/images/dr-calculator.png",
+    href: "/apps/dr-calculator",
   },
 ];
 
